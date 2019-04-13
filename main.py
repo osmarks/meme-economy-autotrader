c42d9df20f92e91e6d6886c22058c1ae72019d25
osmarks <osmarks@protonmail.com>
2019-04-13 23:09:19 +0100

    Add comment ratelimit, use IDs instead of names for efficiency

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from datetime import datetime, timedelta
 import requests
 
 reddit = praw.Reddit()
+
+def id_from_name(name):
+    return reddit.redditor(name).id
+
 meme_economy = reddit.subreddit("MemeEconomy")
 mib_name = "MemeInvestor_bot"
-this_name = "AutoMeme5000"
+mib_id = id_from_name(mib_name)
+bot_name = "AutoMeme5000"
+bot_id = id_from_name(bot_name)
 
 def balance(user):
     bal = requests.get(f"https://meme.market/api/investor/{user}").json()["balance"]
@@ -16,22 +22,33 @@ def balance(user):
 def is_good_investment(score, age, num_comments):
     return age < 60 and age > 1 and num_comments > age and score > age
 
+last_investment = None
+
+def minutes_ago(ev):
+    return (datetime.utcnow() - ev).total_seconds() // 60
+
 def invest(submission):
     for comment in submission.comments:
         # found the investmentbot comment - must reply to this
-        if comment.author.name == mib_name:
+        if comment.author.id == mib_id:
             comment.replies.replace_more(limit=None)
             # check for previous investment by us
             for subcomment in comment.replies:
-                if subcomment.author.name == this_name:
+                if subcomment.author != None and subcomment.author.id == bot_id:
                     print("Already invested in", submission)
                     return
 
-            bal = balance(this_name)
+            global last_investment
+            if last_investment != None and minutes_ago(last_investment) < 11:
+                print("Last investment was", minutes_ago(last_investment), "mins ago (too recent). Waiting...")
+                return
+
+            bal = balance(bot_name)
             if bal < 100:
                 raise RuntimeError("The bot is broke. Please file for bankruptcy.")
             qty = max(bal // 3, 100)
             print("Investing", qty, "in", submission)
+            last_investment = datetime.utcnow()
             comment.reply(f"!invest {qty}")
             return
 
@@ -40,7 +57,7 @@ while True:
     for submission in meme_economy.new():
         if not submission.is_self and not submission.over_18:
             created = datetime.utcfromtimestamp(submission.created)
-            age = (datetime.utcnow() - created).total_seconds() // 60 # age in minutes
+            age = minutes_ago(created)
             if is_good_investment(submission.score, age, submission.num_comments or 0):
                 invest(submission)
 
