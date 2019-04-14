ae0dd3f63ade20bbd9d167f5ab2454221beed32a
osmarks <osmarks@protonmail.com>
2019-04-14 15:13:03 +0100

    Improve logging and bankruptcy handling

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime, timedelta
 import requests
 import shelve
+import logging
 
 shelf = shelve.open("autotrader")
 
@@ -15,10 +16,11 @@ meme_economy = reddit.subreddit("MemeEconomy")
 mib_name = "MemeInvestor_bot"
 bot_name = "AutoMeme5000"
 
-def balance(user):
-    bal = requests.get(f"https://meme.market/api/investor/{user}").json()["balance"]
-    print("Balance is", bal)
-    return bal
+def info(user):
+    data = requests.get(f"https://meme.market/api/investor/{user}").json()
+    logging.info(f"Balance: {data['balance']}.")
+    logging.info(f"Net worth: {data['networth']}.")
+    return data
 
 def is_good_investment(score, age, num_comments):
     return age < 60 and age > 1 and num_comments > age and score > age
@@ -30,7 +32,7 @@ def minutes_ago(ev):
 
 def invest(submission):
     if submission.id in shelf["invested"]:
-        print("Already invested in", submission.id)
+        logging.info(f"Already invested in {submission.id}.")
         return
 
     for comment in submission.comments:
@@ -38,16 +40,20 @@ def invest(submission):
         if comment.author.name == mib_name:
             global last_investment_time
             if last_investment_time != None and minutes_ago(last_investment_time) < 11:
-                print("Last investment was", minutes_ago(last_investment_time), "mins ago (too recent). Waiting...")
+                logging.info(f"Last investment was {minutes_ago(last_investment_time)} minutess ago (too recent). Waiting...")
                 return
 
-            bal = balance(bot_name)
-            if bal < 100:
-                raise RuntimeError("The bot is broke. Please file for bankruptcy.")
-            qty = max(bal // 3, 100)
-            print("Investing", qty, "in", submission)
+            data = info(bot_name)
+            if data["balance"] < 100:
+                logging.warning(f"Not enough money to invest. Waiting...")
+                if data["networth"] < 100:
+                    raise RuntimeError("The bot is broke (balance and possible value of investments below 100). Please file for bankruptcy.")
+                return
+
+            qty = max(data["balance"] // 3, 100)
             last_investment_time = datetime.utcnow()
             comment.reply(f"!invest {qty}")
+            logging.info(f"Invested {qty} in {submission.id}")
 
             invested_list = shelf["invested"]
             invested_list.add(submission.id)
@@ -55,8 +61,10 @@ def invest(submission):
 
             return
 
+logging.basicConfig(level=logging.INFO, format="%(levelname)s %(asctime)s %(message)s", datefmt="%H:%M:%S %d/%m/%Y")
+
 while True:
-    print("Running meme check cycle")
+    logging.info("Running meme check cycle.")
     for submission in meme_economy.new():
         if not submission.is_self and not submission.over_18:
             created = datetime.utcfromtimestamp(submission.created)
